b6bc8b2aa77fe805facc6c62278d544bf3340896
Arturo Trujillo <57078913+arturo-trujillo@users.noreply.github.com>
2021-11-13 01:59:41 -0600

    Simple API with Nodejs

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+//app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`Up n Running at http://localhost:${PORT} `)
+);
+
+app.get('/component', (req,res)=>{
+    res.status(200).send({
+        start:'Hello ',
+        middle: 'I am ',
+        end: 'Alive!'
+    })
+});
+
+// This should work, but i dont wanna use Insomnia nor Postman XD
+
+app.post('/component/:id',(req,res)=> {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    if(!name){
+       res.status(418).send({message: 'Start is required!'});
+    }
+    res.send({
+        name: `Component with your ${start} and ID of ${id}`
+    });
+
+});
+
+
+// htttps://swagger.io/specification/ 
+// MUST LOOK AT THIS LINK!
